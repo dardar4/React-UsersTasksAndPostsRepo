b57009f48e2b81b0a1dad493cf0853828fb4f763
Amir <dardar4@gmail.com>
2020-09-08 23:14:55 +0300

    Updates

---
+++ precache-manifest.06562646111be0145fbd29ccb5367bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3dc52f4925695d22c1787f80d043988",
+    "url": "/React-UsersTasksAndPostsRepo/index.html"
+  },
+  {
+    "revision": "4ce2d9c34f07a6010505",
+    "url": "/React-UsersTasksAndPostsRepo/static/css/main.29787244.chunk.css"
+  },
+  {
+    "revision": "aa238743bd9dc4ac7458",
+    "url": "/React-UsersTasksAndPostsRepo/static/js/2.644ac4c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-UsersTasksAndPostsRepo/static/js/2.644ac4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce2d9c34f07a6010505",
+    "url": "/React-UsersTasksAndPostsRepo/static/js/main.da3bafc3.chunk.js"
+  },
+  {
+    "revision": "4195e55b04bb37b876e6",
+    "url": "/React-UsersTasksAndPostsRepo/static/js/runtime-main.c33807bd.js"
+  }
+]);
